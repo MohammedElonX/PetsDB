85fee6f28e321c3ec67bd18f8473b65f04f347f1
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-01-14 17:08:44 +0100

    Added data package with PetContract class

--- app/src/main/java/com/example/petsdb/EditorActivity.java
+++ app/src/main/java/com/example/petsdb/EditorActivity.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import com.example.petsdb.data.PetsContract.PetEntry;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -68,11 +69,11 @@ public class EditorActivity extends AppCompatActivity {
                 String selection = (String) parent.getItemAtPosition(position);
                 if (!TextUtils.isEmpty(selection)) {
                     if (selection.equals(getString(R.string.gender_male))) {
-                        mGender = 1; // Male
+                        mGender = PetEntry.GENDER_MALE; // Male
                     } else if (selection.equals(getString(R.string.gender_female))) {
-                        mGender = 2; // Female
+                        mGender = PetEntry.GENDER_FEMALE; // Female
                     } else {
-                        mGender = 0; // Unknown
+                        mGender = PetEntry.GENDER_UNKNOWN; // Unknown
                     }
                 }
             }
