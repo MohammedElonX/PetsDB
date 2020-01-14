ae8f9127073138ba0f54fbdf2d7cbeb95578f7a0
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-01-14 18:39:15 +0100

    Added displayDATABASEiNFO method

--- app/src/main/java/com/example/petsdb/MainActivity.java
+++ app/src/main/java/com/example/petsdb/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.petsdb;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +11,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import com.example.petsdb.data.HelperDB;
+import com.example.petsdb.data.PetsContract;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolBar;
@@ -29,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        displayDataBaseInfo();
+    }
+
+    public void displayDataBaseInfo(){
+        HelperDB mHelperDB = new HelperDB(this);
+        SQLiteDatabase sqLiteDatabase = mHelperDB.getReadableDatabase();
+        //String[] tb = PetsContract.PetEntry.TABLE_NAME.split("");
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM", PetsContract.PetEntry.TABLE_NAME, null);
+
+        try{
+            TextView display = findViewById(R.id.text_view_pet);
+            display.setText("Number of rows in pets table" + cursor.getCount());
+        } finally {
+            cursor.close();
+        }
     }
 
     @Override
