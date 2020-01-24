884b347d8b02988e44a456f1fe1c1d1f643bd638
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-01-24 20:54:39 +0100

    Added cursorAdapter and id to list view

---
+++ app/src/main/java/com/example/petsdb/PetCursorAdapter.java
@@ -0,0 +1,25 @@
+package com.example.petsdb;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+public class PetCursorAdapter extends CursorAdapter {
+
+    PetCursorAdapter(Context context, Cursor c){
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item ,parent ,false) ;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
